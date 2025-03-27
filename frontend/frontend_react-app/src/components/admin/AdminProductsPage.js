ecff8cabf0236a5cd2d03297dfee920a4b47693c
Diogo Passos <diogopassos91@gmail.com>
2025-03-27 16:27:02 +0000

    Gestão de Produtos --> Filtrar por categorias

--- frontend/frontend_react-app/src/components/admin/AdminProductsPage.js
+++ frontend/frontend_react-app/src/components/admin/AdminProductsPage.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AddCategoryModal from '../admin/AddCategoryModal';
+import FilterProductsByCategory from '../admin/FilterProductsByCategory'; // Importar o modal de filtro
+
 
 function AdminProductsPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAddCategoryModalOpen, setIsAddCategoryModalOpen] = useState(false);
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false); // Novo estado para o filtro
   const navigate = useNavigate();
 
   const handleNavigateToInativos = () => {
@@ -14,13 +17,13 @@ function AdminProductsPage() {
     <div>
       <h2>Gestão de Produtos - Área Administrativa</h2>
       <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-        <button className="btn-primary" onClick={() => setIsModalOpen(true)}>
+        <button className="btn-primary" onClick={() => setIsAddCategoryModalOpen(true)}>
           Adicionar Categoria
         </button>
         <button className="btn-primary" onClick={handleNavigateToInativos}>
           Listar Produtos Inativos
         </button>
-        <button className="btn-primary" onClick={() => navigate('/admin/produtos/filtrar-categoria')}>
+        <button className="btn-primary" onClick={() => setIsFilterModalOpen(true)}>
           Filtrar por Categoria
         </button>
         <button className="btn-primary" onClick={() => navigate('/admin/produtos/filtrar-vendedor')}>
@@ -28,13 +31,22 @@ function AdminProductsPage() {
         </button>
       </div>
 
-      {/* Modal */}
-      {isModalOpen && (
+      {/* Modais */}
+      {isAddCategoryModalOpen && (
         <div style={{ zIndex: 9999 }}>
           <AddCategoryModal 
-            isOpen={isModalOpen} 
-            onClose={() => setIsModalOpen(false)} 
+            isOpen={isAddCategoryModalOpen} 
+            onClose={() => setIsAddCategoryModalOpen(false)} 
             onCategoryAdded={() => console.log('Categoria adicionada!')} 
+          />
+        </div>
+      )}
+
+      {isFilterModalOpen && (
+        <div style={{ zIndex: 9999 }}>
+          <FilterProductsByCategory 
+            isOpen={isFilterModalOpen} 
+            onClose={() => setIsFilterModalOpen(false)} 
           />
         </div>
       )}
--- frontend/frontend_react-app/src/components/admin/FilterProductsByCategory.js
+++ frontend/frontend_react-app/src/components/admin/FilterProductsByCategory.js
@@ -90,3 +90,4 @@ function FilterProductsByCategory({ isOpen, onClose, onFilter }) {
 
 export default FilterProductsByCategory;
 
+
