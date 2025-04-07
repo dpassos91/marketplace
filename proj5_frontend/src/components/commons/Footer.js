22c71e8d63ffd0f1d0ea9a59578121fef83430cb
Diogo Passos <diogopassos91@gmail.com>
2025-04-07 16:01:10 +0100

    Int --> footer

--- proj5_frontend/src/components/commons/Footer.js
+++ proj5_frontend/src/components/commons/Footer.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './Footer.css'; // Certifique-se de que o caminho está correto
+import { FormattedMessage } from 'react-intl';
 
 function Footer() {
   return (
     <footer id="footer">
       <div className="direitos_autor">
-        <p>© 2025 | Todos os direitos reservados</p>
+        <p><FormattedMessage id="footer.rights" defaultMessage="© 2025 | Todos os direitos reservados"/></p>
       </div>
 
       <div>
-        <h4>Diogo Passos | Projeto 5 | AOR - PAJ 2025</h4>
+        <h4><FormattedMessage id="footer.author" defaultMessage="Diogo Passos | Projeto 5 | AOR - PAJ 2025"/></h4>
       </div>
 
       <div className="centro_atendimento">
-        <h4>Centro de Atendimento</h4>
-        <p>Atendimento: Seg-Sex, 08h - 18h</p>
-        <p>Telefone: +351 || 239 123 456</p>
+        <h4><FormattedMessage id="footer.center" defaultMessage="Centro de Atendimento"/></h4>
+        <p><FormattedMessage id="footer.service" defaultMessage="Seg-Sex, 08h - 18h"/></p>
+        <p><FormattedMessage id="footer.phone" defaultMessage="Telefone: +351 || 239 123 456"/></p>
         <p>E-mail: 
           <a href="mailto:suporte@loop-market.com">suporte@loop-market.com</a>
         </p>
