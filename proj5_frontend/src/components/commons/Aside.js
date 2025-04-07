4e5f5cbe432d2186675ec01eaa8e0240f9f9c86c
Diogo Passos <diogopassos91@gmail.com>
2025-04-07 17:01:25 +0100

    Int --> aside

--- proj5_frontend/src/components/commons/Aside.js
+++ proj5_frontend/src/components/commons/Aside.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Aside.css'; // Certifique-se de que o caminho está correto
+import { FormattedMessage } from 'react-intl';
 
 function Aside() {
   return (
@@ -8,19 +9,25 @@ function Aside() {
       <div className="nav-pag">
         <Link to="/produtos">
           <button className="btn-produtos" type="button">
-            <h2>Produtos</h2>
+            <h2><FormattedMessage id="aside.products" defaultMessage="Produtos" /></h2>
           </button>
         </Link>
       </div>
       <aside>
         <div className="sobre_nos">
-          <h1>Sobre Nós</h1>
+          <h1><FormattedMessage id="aside.aboutUs" defaultMessage="Sobre Nós" /></h1>
           <p>
-            No Loop Market, promovemos a economia circular, reutilizando e reciclando recursos para minimizar o desperdício.
-            Nossa plataforma conecta consumidores e empresas a produtos sustentáveis, prolongando o ciclo de vida dos materiais
-            e reduzindo o impacto ambiental.
+            <FormattedMessage
+              id="aside.description"
+              defaultMessage="No Loop Market, promovemos a economia circular, reutilizando e reciclando recursos para minimizar o desperdício.
+              Nossa plataforma conecta consumidores e empresas a produtos sustentáveis, prolongando o ciclo de vida dos materiais
+              e reduzindo o impacto ambiental."
+            />
             <br /><br />
-            Cada compra no Loop Market apoia um consumo mais consciente e ecológico. Junte-se a nós e contribua para um futuro mais verde e sustentável.
+            <FormattedMessage
+              id="aside.callToAction"
+              defaultMessage="Cada compra no Loop Market apoia um consumo mais consciente e ecológico. Junte-se a nós e contribua para um futuro mais verde e sustentável."
+            />
           </p>
         </div>
       </aside>
