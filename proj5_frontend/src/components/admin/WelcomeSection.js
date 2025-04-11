788271e6b63dbf2706a94a095f03f1064fb8342c
Diogo Passos <diogopassos91@gmail.com>
2025-04-11 14:59:22 +0100

    Int --> WelcomeSection

--- proj5_frontend/src/components/admin/WelcomeSection.js
+++ proj5_frontend/src/components/admin/WelcomeSection.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import  './WelcomeSection.css';
 
 function WelcomeSection() {
   return (
     <section id="welcome-section" className="welcome-section" style={{ textAlign: 'center' }}>
-      <h1>Bem-vindo ao Painel de Administração!</h1>
-      <p style={{ marginTop: '20px' }}>Selecione uma opção no menu à esquerda para começar.</p>
-      <p style={{ marginTop: '20px' }}>Aqui poderá gerir avaliações, produtos e utilizadores do sistema.</p>
+      <h1>
+        <FormattedMessage id="admin.welcomeSection.title" defaultMessage="Bem-vindo ao Painel de Administração!" />
+      </h1>
+      <p>
+        <FormattedMessage id="admin.welcomeSection.instruction" defaultMessage="Selecione uma opção no menu à esquerda para começar." />
+      </p>
+      <p>
+        <FormattedMessage id="admin.welcomeSection.description" defaultMessage="Aqui poderá gerir avaliações, produtos e utilizadores do sistema." />
+      </p>
     </section>
   );
 }
 
 export default WelcomeSection;
+
 
