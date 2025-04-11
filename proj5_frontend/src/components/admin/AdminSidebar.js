e7c71c26c47d8b01061f0d9a107bc133cfed9341
Diogo Passos <diogopassos91@gmail.com>
2025-04-11 15:08:47 +0100

    Int --> AdminSidebar

--- proj5_frontend/src/components/admin/AdminSidebar.js
+++ proj5_frontend/src/components/admin/AdminSidebar.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 
 function AdminSidebar() {
   return (
     <aside className="admin-sidebar">
-      <h2>Painel de Administração</h2>
+      <h2>
+        <FormattedMessage id="admin.sidebar.title" defaultMessage="Painel de Administração" />
+      </h2>
       <nav>
         <ul>
           <li>
+            <Link to="/admin/dashboard" className="btn-produtos" id="dashboard">
+              <FormattedMessage id="admin.sidebar.dashboard" defaultMessage="Dashboard" />
+            </Link>
+          </li>
+          <li>
             <Link to="/admin/avaliacoes" className="btn-produtos" id="gestao-avaliacoes">
-              Gestão de Avaliações
+              <FormattedMessage id="admin.sidebar.avaliacoes" defaultMessage="Gestão de Avaliações" />
             </Link>
           </li>
           <li>
             <Link to="/admin/produtos" className="btn-produtos" id="gestao-produtos">
-              Gestão de Produtos
+              <FormattedMessage id="admin.sidebar.produtos" defaultMessage="Gestão de Produtos" />
             </Link>
           </li>
           <li>
             <Link to="/admin/utilizadores" className="btn-produtos" id="gestao-utilizadores">
-              Gestão de Utilizadores
+              <FormattedMessage id="admin.sidebar.utilizadores" defaultMessage="Gestão de Utilizadores" />
             </Link>
           </li>
         </ul>
@@ -29,4 +37,5 @@ function AdminSidebar() {
 }
 
 export default AdminSidebar;
+
 
