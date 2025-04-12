5de5c49f764741ea6e89f2eb7e2c65c0bafa6107
Diogo Passos <diogopassos91@gmail.com>
2025-04-12 11:29:05 +0100

    Int --> AdminProductsPage

--- proj5_frontend/src/components/admin/AdminProductsPage.js
+++ proj5_frontend/src/components/admin/AdminProductsPage.js
@@ -4,6 +4,7 @@ import AddCategoryModal from '../admin/AddCategoryModal';
 import FilterProductsByCategory from '../admin/FilterProductsByCategory';
 import FilterProductsBySeller from '../admin/FilterProductsBySeller';
 import './AdminProductsPage.css';
+import { FormattedMessage } from 'react-intl';
 
 function AdminProductsPage() {
   const [isAddCategoryModalOpen, setIsAddCategoryModalOpen] = useState(false);
@@ -21,31 +22,34 @@ function AdminProductsPage() {
 
   return (
     <div>
-      <h2 className="admin-title">Gestão de Produtos</h2>
+      <h2 className="admin-title">
+        <FormattedMessage id="admin.products.title" defaultMessage="Gestão de Produtos" />
+      </h2>
 
       <div className="admin-actions-grid">
         <div className="admin-actions-column">
           <button className="btn-card tabela-btn btn-danger" onClick={() => setIsAddCategoryModalOpen(true)}>
-            Adicionar categoria
+            <FormattedMessage id="admin.products.addCategory" defaultMessage="Adicionar categoria" />
           </button>
           <button className="btn-card tabela-btn btn-danger" onClick={() => setIsFilterModalOpen(true)}>
-            Filtrar por categoria
+            <FormattedMessage id="admin.products.filterByCategory" defaultMessage="Filtrar por categoria" />
           </button>
         </div>
 
         <div className="admin-actions-column">
           <button className="btn-card tabela-btn btn-danger" onClick={() => setIsFilterBySellerModalOpen(true)}>
-            Filtrar por vendedor
+            <FormattedMessage id="admin.products.filterBySeller" defaultMessage="Filtrar por vendedor" />
           </button>
           <button className="btn-card tabela-btn btn-danger" onClick={handleNavigateToInativos}>
-            Listar produtos inativos
+            <FormattedMessage id="admin.products.listInactive" defaultMessage="Listar produtos inativos" />
           </button>
         </div>
       </div>
 
+      {/* Botão central */}
       <div className="admin-actions-center">
         <button className="btn-card tabela-btn btn-danger" onClick={handleNavigateToAlterados}>
-          Mostrar produtos alterados
+          <FormattedMessage id="admin.products.showModified" defaultMessage="Mostrar produtos alterados" />
         </button>
       </div>
 
@@ -82,3 +86,4 @@ function AdminProductsPage() {
 }
 
 export default AdminProductsPage;
+
