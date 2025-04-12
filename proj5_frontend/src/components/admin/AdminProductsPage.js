4e665098eed87dfffc8c7a2828fe9419f52a50f9
Diogo Passos <diogopassos91@gmail.com>
2025-04-12 11:22:28 +0100

    CSS --> ajustes AdminProductsPage

--- proj5_frontend/src/components/admin/AdminProductsPage.js
+++ proj5_frontend/src/components/admin/AdminProductsPage.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AddCategoryModal from '../admin/AddCategoryModal';
-import FilterProductsByCategory from '../admin/FilterProductsByCategory'; // Importar o modal de filtro por categoria
-import FilterProductsBySeller from '../admin/FilterProductsBySeller'; // Importar o modal de filtro por vendedor
-import './AdminProductsPage.css'; // Importar o CSS para estilização
+import FilterProductsByCategory from '../admin/FilterProductsByCategory';
+import FilterProductsBySeller from '../admin/FilterProductsBySeller';
+import './AdminProductsPage.css';
 
 function AdminProductsPage() {
   const [isAddCategoryModalOpen, setIsAddCategoryModalOpen] = useState(false);
-  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false); // Novo estado para o filtro por categoria
-  const [isFilterBySellerModalOpen, setIsFilterBySellerModalOpen] = useState(false); // Novo estado para o filtro por vendedor
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
+  const [isFilterBySellerModalOpen, setIsFilterBySellerModalOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleNavigateToInativos = () => {
@@ -22,54 +22,58 @@ function AdminProductsPage() {
   return (
     <div>
       <h2 className="admin-title">Gestão de Produtos</h2>
-      <div className="admin-actions-grid">
-  <div className="admin-actions-column">
-    <button className="btn-card tabela-btn btn-danger" onClick={() => setIsAddCategoryModalOpen(true)}>
-      Adicionar categoria
-    </button>
-    <button className="btn-card tabela-btn btn-danger" onClick={handleNavigateToInativos}>
-      Listar produtos inativos
-    </button>
-    <button className="btn-card tabela-btn btn-danger" onClick={() => setIsFilterModalOpen(true)}>
-      Filtrar por categoria
-    </button>
-  </div>
-  <div className="admin-actions-column">
-    <button className="btn-card tabela-btn btn-danger" onClick={() => setIsFilterBySellerModalOpen(true)}>
-      Filtrar por vendedor
-    </button>
-    <button className="btn-card tabela-btn btn-danger" onClick={handleNavigateToAlterados}>
-      Mostrar produtos alterados
-    </button>
-  </div>
-</div>
 
+      <div className="admin-actions-grid">
+        <div className="admin-actions-column">
+          <button className="btn-card tabela-btn btn-danger" onClick={() => setIsAddCategoryModalOpen(true)}>
+            Adicionar categoria
+          </button>
+          <button className="btn-card tabela-btn btn-danger" onClick={() => setIsFilterModalOpen(true)}>
+            Filtrar por categoria
+          </button>
+        </div>
+
+        <div className="admin-actions-column">
+          <button className="btn-card tabela-btn btn-danger" onClick={() => setIsFilterBySellerModalOpen(true)}>
+            Filtrar por vendedor
+          </button>
+          <button className="btn-card tabela-btn btn-danger" onClick={handleNavigateToInativos}>
+            Listar produtos inativos
+          </button>
+        </div>
+      </div>
+
+      <div className="admin-actions-center">
+        <button className="btn-card tabela-btn btn-danger" onClick={handleNavigateToAlterados}>
+          Mostrar produtos alterados
+        </button>
+      </div>
 
       {/* Modais */}
       {isAddCategoryModalOpen && (
         <div style={{ zIndex: 9999 }}>
-          <AddCategoryModal 
-            isOpen={isAddCategoryModalOpen} 
-            onClose={() => setIsAddCategoryModalOpen(false)} 
-            onCategoryAdded={() => console.log('Categoria adicionada!')} 
+          <AddCategoryModal
+            isOpen={isAddCategoryModalOpen}
+            onClose={() => setIsAddCategoryModalOpen(false)}
+            onCategoryAdded={() => console.log('Categoria adicionada!')}
           />
         </div>
       )}
 
       {isFilterModalOpen && (
         <div style={{ zIndex: 9999 }}>
-          <FilterProductsByCategory 
-            isOpen={isFilterModalOpen} 
-            onClose={() => setIsFilterModalOpen(false)} 
+          <FilterProductsByCategory
+            isOpen={isFilterModalOpen}
+            onClose={() => setIsFilterModalOpen(false)}
           />
         </div>
       )}
 
       {isFilterBySellerModalOpen && (
         <div style={{ zIndex: 9999 }}>
-          <FilterProductsBySeller 
-            isOpen={isFilterBySellerModalOpen} 
-            onClose={() => setIsFilterBySellerModalOpen(false)} 
+          <FilterProductsBySeller
+            isOpen={isFilterBySellerModalOpen}
+            onClose={() => setIsFilterBySellerModalOpen(false)}
           />
         </div>
       )}
