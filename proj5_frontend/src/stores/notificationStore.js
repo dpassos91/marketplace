f4fb3b5ceb7407cbe04d6d9c66a433488bbf2ee9
Diogo Passos <diogopassos91@gmail.com>
2025-04-24 09:02:07 +0100

    notificationStore --> persistência de notificações em localStorage para pemitir que as notificações se mantenham depois do refresh

--- proj5_frontend/src/stores/notificationStore.js
+++ proj5_frontend/src/stores/notificationStore.js
@@ -1,31 +1,39 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const notificationStore = create((set) => ({
-  notifications: [],
-  unreadCount: 0,
-  chatUser: null,
-
-  addNotification: (notification) =>
-    set((state) => ({
-      notifications: [...state.notifications, notification],
-      unreadCount: state.unreadCount + 1,
-    })),
-
-  markAllAsRead: () =>
-    set((state) => ({
-      notifications: state.notifications.map((n) => ({
-        ...n,
-        read: true
-      })),
-      unreadCount: 0,
-    })),
-
-  clearNotifications: () =>
-    set({
+export const notificationStore = create(
+  persist(
+    (set) => ({
       notifications: [],
       unreadCount: 0,
-    }),
+      chatUser: null,
 
-  openChatWith: (username) => set({ chatUser: username }),
-  closeChat: () => set({ chatUser: null }),
-}));
+      addNotification: (notification) =>
+        set((state) => ({
+          notifications: [...state.notifications, notification],
+          unreadCount: state.unreadCount + 1,
+        })),
+
+      markAllAsRead: () =>
+        set((state) => ({
+          notifications: state.notifications.map((n) => ({
+            ...n,
+            read: true,
+          })),
+          unreadCount: 0,
+        })),
+
+      clearNotifications: () =>
+        set({
+          notifications: [],
+          unreadCount: 0,
+        }),
+
+      openChatWith: (username) => set({ chatUser: username }),
+      closeChat: () => set({ chatUser: null }),
+    }),
+    {
+      name: 'notification-store', // chave no localStorage
+    }
+  )
+);
